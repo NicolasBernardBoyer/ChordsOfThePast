0b550448052663800f9ab95f720a56aa6b95ac65
Abd Alwahed Haj Omar <abdolking22@gmail.com>
2025-01-12 12:00:16 -0500

    TuneHarpSceneReady

--- Chords of the Past/Assets/Scripts/HarpTuningScripts/HarpController.cs
+++ Chords of the Past/Assets/Scripts/HarpTuningScripts/HarpController.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HarpController : MonoBehaviour
 {
@@ -20,6 +22,9 @@ public class HarpController : MonoBehaviour
 
     [SerializeField] private AudioSource[] cordSounds = new AudioSource[4];
 
+    [SerializeField] private Slider pitchSlider; // Slider to control pitch
+    [SerializeField] private Button evaluateButton;
+
     private GameObject selectedCord;
 
     private void Awake()
@@ -33,6 +38,12 @@ public class HarpController : MonoBehaviour
             {
                 sound.pitch = Random.Range(0.5f, 2f);
             }
+        }
+
+        // Link button to EvaluateAndProceed
+        if (evaluateButton != null)
+        {
+            evaluateButton.onClick.AddListener(EvaluateAndProceed);
         }
     }
 
@@ -61,6 +72,9 @@ public class HarpController : MonoBehaviour
         fourthCordAction.performed += context => SelectCord(fourthCord);
         playSongAction.performed += PlaySong;
         rotationAction.performed += RotateCord;
+
+        // Bind slider value change event
+        pitchSlider.onValueChanged.AddListener(OnPitchSliderChanged);
     }
 
     private void OnDisable()
@@ -71,6 +85,9 @@ public class HarpController : MonoBehaviour
         fourthCordAction.Disable();
         playSongAction.Disable();
         rotationAction.Disable();
+
+        // Remove slider listener
+        pitchSlider.onValueChanged.RemoveListener(OnPitchSliderChanged);
     }
 
     private void SelectCord(GameObject cord)
@@ -82,6 +99,13 @@ public class HarpController : MonoBehaviour
         // Add border to selected cord
         var border = selectedCord.transform.Find("Border");
         if (border != null) border.gameObject.SetActive(true);
+
+        // Update slider to match the pitch of the selected cord
+        int cordIndex = GetCordIndex(selectedCord);
+        if (cordIndex != -1 && cordSounds[cordIndex] != null)
+        {
+            pitchSlider.value = cordSounds[cordIndex].pitch; // Set slider value
+        }
     }
 
     private void ResetCordBorders()
@@ -116,6 +140,9 @@ public class HarpController : MonoBehaviour
             float currentRotation = selectedCord.transform.eulerAngles.z;
             float normalizedPitch = Mathf.Lerp(0.5f, 2f, (currentRotation % 360) / 360f);
             AdjustPitch(normalizedPitch, cordIndex);
+
+            // Update slider to reflect new pitch
+            pitchSlider.value = cordSounds[cordIndex].pitch;
         }
     }
 
@@ -161,4 +188,38 @@ public class HarpController : MonoBehaviour
             Debug.LogError($"AudioSource for cord {cordIndex} is not assigned!");
         }
     }
+
+    private void OnPitchSliderChanged(float newPitch)
+    {
+        if (selectedCord == null) return;
+
+        int cordIndex = GetCordIndex(selectedCord);
+        if (cordIndex != -1)
+        {
+            AdjustPitch(newPitch, cordIndex);
+        }
+    }
+
+    private void EvaluateAndProceed()
+    {
+        float targetPitch = 1.25f;
+        float totalScore = 0f;
+
+        foreach (var sound in cordSounds)
+        {
+            if (sound != null)
+            {
+                float pitchDifference = Mathf.Abs(sound.pitch - targetPitch);
+                float score = Mathf.Clamp(pitchDifference * 100, 0, 100); // Scaled difference
+                totalScore += score;
+            }
+        }
+
+        Debug.Log($"Total Score (Lower is Better): {totalScore}");
+
+        // Proceed to the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
 }
+
